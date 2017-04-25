03bbaa5d9e3be8b3901344987725bb7bf3194169
root <root@jenkins.lab>
2017-04-25 16:39:43 +0200

    disabled build

--- pushtests.sh
+++ pushtests.sh
@@ -15,33 +15,33 @@ declare -A vmcopy # declare an associative array to copy our VM array into
 for I in "${TEST_VM_LIST[@]}"; do vmcopy[$I]=$I; done
 
 WAIT=0
-while [[ ${#vmcopy[@]} -gt 0 ]]
-do
-    sleep 10
-    ((WAIT+=10))
-    info "Waiting 10 seconds"
-    for I in "${vmcopy[@]}"
-    do
-        echo -n "Checking if test server $I has rebuilt... (Sat 6.2 style)"
-        status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
-            "hammer host info --name $I | \
-            grep -e \"Managed.*yes\" -e \"Enabled.*yes\" -e \"Build.*no\" \
-		| wc -l")
-        # Check if status is OK, ping reacts and SSH is there, then success!
-        if [[ ${status} == 3 ]] && ping -c 1 -q $I && nc -w 1 $I 22
-        then
-            tell "Success!"
-            unset vmcopy[$I]
-        else
-            tell "Not yet."
-        fi
-    done
-    if [[ ${WAIT} -gt 6000 ]]
-    then
-        err "Test servers still not rebuilt after 6000 seconds. Exiting."
-        exit 1
-    fi
-done
+#while [[ ${#vmcopy[@]} -gt 0 ]]
+#do
+#    sleep 10
+#    ((WAIT+=10))
+#    info "Waiting 10 seconds"
+#    for I in "${vmcopy[@]}"
+#    do
+#        echo -n "Checking if test server $I has rebuilt... (Sat 6.2 style)"
+#        status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+#            "hammer host info --name $I | \
+#            grep -e \"Managed.*yes\" -e \"Enabled.*yes\" -e \"Build.*no\" \
+#		| wc -l")
+#        # Check if status is OK, ping reacts and SSH is there, then success!
+#        if [[ ${status} == 3 ]] && ping -c 1 -q $I && nc -w 1 $I 22
+#        then
+#            tell "Success!"
+#            unset vmcopy[$I]
+#        else
+#            tell "Not yet."
+#        fi
+#    done
+#    if [[ ${WAIT} -gt 6000 ]]
+#    then
+#        err "Test servers still not rebuilt after 6000 seconds. Exiting."
+#        exit 1
+#    fi
+#done
 
 # Wait another 30s to be on the safe side
 sleep 30
